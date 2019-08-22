4268ee4cb026c696b49a6f8f000bd3850dae3825
Loic Nageleisen <loic@sqreen.io>
2019-08-22 17:59:26 +0200

    Isolate Sqreen changes for extconf

--- ext/mini_racer_extension/extconf.rb
+++ ext/mini_racer_extension/extconf.rb
@@ -1,64 +1,23 @@
 require 'mkmf'
+
 require 'fileutils'
 
-IS_DARWIN = RUBY_PLATFORM =~ /darwin/
 IS_SOLARIS = RUBY_PLATFORM =~ /solaris/
 
-have_library('pthread')
-have_library('objc') if IS_DARWIN
-$CPPFLAGS.gsub! /-std=[^\s]+/, ''
-$CPPFLAGS += " -Wall" unless $CPPFLAGS.split.include? "-Wall"
-$CPPFLAGS += " -g" unless $CPPFLAGS.split.include? "-g"
-$CPPFLAGS += " -rdynamic" unless $CPPFLAGS.split.include? "-rdynamic"
-$CPPFLAGS += " -fPIC" unless $CPPFLAGS.split.include? "-rdynamic" or IS_DARWIN
-$CPPFLAGS += " -std=c++0x"
-$CPPFLAGS += " -fpermissive"
-$CPPFLAGS += " -fno-omit-frame-pointer"
-if enable_config('avx2')
-  $CPPFLAGS += " -mavx2"
-else
-  $CPPFLAGS += " -mssse3"
+def cppflags_clear_std!
+  $CPPFLAGS.gsub! /-std=[^\s]+/, ''
 end
 
-$CPPFLAGS += " -Wno-reserved-user-defined-literal" if IS_DARWIN
-
-$LDFLAGS.insert(0, " -stdlib=libc++ ") if IS_DARWIN
-
-if ENV['CXX']
-  puts "SETTING CXX"
-  CONFIG['CXX'] = ENV['CXX']
+def cppflags_add_frame_pointer!
+  $CPPFLAGS += " -fno-omit-frame-pointer"
 end
 
-CXX11_TEST = <<EOS
-#if __cplusplus <= 199711L
-#   error A compiler that supports at least C++11 is required in order to compile this project.
-#endif
-EOS
-
-`echo "#{CXX11_TEST}" | #{CONFIG['CXX']} -std=c++0x -x c++ -E -`
-unless $?.success?
-  warn <<EOS
-
-
-WARNING: C++11 support is required for compiling mini_racer. Please make sure
-you are using a compiler that supports at least C++11. Examples of such
-compilers are GCC 4.7+ and Clang 3.2+.
-
-If you are using Travis, consider either migrating your build to Ubuntu Trusty or
-installing GCC 4.8. See mini_racer's README.md for more information.
-
-
-EOS
-end
-
-CONFIG['LDSHARED'] = '$(CXX) -shared' unless IS_DARWIN
-if CONFIG['warnflags']
-  CONFIG['warnflags'].gsub!('-Wdeclaration-after-statement', '')
-  CONFIG['warnflags'].gsub!('-Wimplicit-function-declaration', '')
-end
-
-if enable_config('debug') || enable_config('asan')
-  CONFIG['debugflags'] << ' -ggdb3 -O0'
+def cppflags_add_cpu_extension!
+  if enable_config('avx2')
+    $CPPFLAGS += " -mavx2"
+  else
+    $CPPFLAGS += " -mssse3"
+  end
 end
 
 def fixup_libtinfo
@@ -121,41 +80,104 @@ EOS
   "#{ruby} '#{file.path}'"
 end
 
-LIBV8_VERSION = '7.3.492.27.1'
-libv8_glob = "**/*-#{LIBV8_VERSION}-*/**/libv8.rb"
-libv8_rb = Dir.glob(libv8_glob).first
-FileUtils.mkdir_p('gemdir')
-unless libv8_rb
-  gem_name = libv8_gem_name
-  cmd = "#{fixup_libtinfo} #{force_platform_gem} install --version '= #{LIBV8_VERSION}' --install-dir gemdir #{gem_name}"
-  puts "Will try downloading #{gem_name} gem: #{cmd}"
-  `#{cmd}`
-  unless $?.success?
-    warn <<EOS
+def libv8_version
+  '7.3.492.27.1'
+end
+
+def find_libv8
+  libv8_glob = "**/*-#{libv8_version}-*/**/libv8.rb"
+  libv8_rb = Dir.glob(libv8_glob).first
+  FileUtils.mkdir_p('gemdir')
+  unless libv8_rb
+    gem_name = libv8_gem_name
+    cmd = "#{fixup_libtinfo} #{force_platform_gem} install --version '= #{libv8_version}' --install-dir gemdir #{gem_name}"
+    puts "Will try downloading #{gem_name} gem: #{cmd}"
+    `#{cmd}`
+    unless $?.success?
+      warn <<EOS
 
 WARNING: Could not download a private copy of the libv8 gem. Please make
 sure that you have internet access and that the `gem` binary is available.
 
 EOS
-  end
+    end
 
-  libv8_rb = Dir.glob(libv8_glob).first
-  unless libv8_rb
-    warn <<EOS
+    libv8_rb = Dir.glob(libv8_glob).first
+    unless libv8_rb
+      warn <<EOS
 
 WARNING: Could not find libv8 after the local copy of libv8 having supposedly
 been installed.
 
 EOS
+    end
+  end
+
+  if libv8_rb
+    $:.unshift(File.dirname(libv8_rb) + '/../ext')
+    $:.unshift File.dirname(libv8_rb)
   end
 end
 
-if libv8_rb
-  $:.unshift(File.dirname(libv8_rb) + '/../ext')
-  $:.unshift File.dirname(libv8_rb)
-end
+find_libv8
 
 require 'libv8'
+
+IS_DARWIN = RUBY_PLATFORM =~ /darwin/
+
+have_library('pthread')
+have_library('objc') if IS_DARWIN
+cppflags_clear_std!
+$CPPFLAGS += " -Wall" unless $CPPFLAGS.split.include? "-Wall"
+$CPPFLAGS += " -g" unless $CPPFLAGS.split.include? "-g"
+$CPPFLAGS += " -rdynamic" unless $CPPFLAGS.split.include? "-rdynamic"
+$CPPFLAGS += " -fPIC" unless $CPPFLAGS.split.include? "-rdynamic" or IS_DARWIN
+$CPPFLAGS += " -std=c++0x"
+$CPPFLAGS += " -fpermissive"
+cppflags_add_frame_pointer!
+cppflags_add_cpu_extension!
+
+$CPPFLAGS += " -Wno-reserved-user-defined-literal" if IS_DARWIN
+
+$LDFLAGS.insert(0, " -stdlib=libc++ ") if IS_DARWIN
+
+if ENV['CXX']
+  puts "SETTING CXX"
+  CONFIG['CXX'] = ENV['CXX']
+end
+
+CXX11_TEST = <<EOS
+#if __cplusplus <= 199711L
+#   error A compiler that supports at least C++11 is required in order to compile this project.
+#endif
+EOS
+
+`echo "#{CXX11_TEST}" | #{CONFIG['CXX']} -std=c++0x -x c++ -E -`
+unless $?.success?
+  warn <<EOS
+
+
+WARNING: C++11 support is required for compiling mini_racer. Please make sure
+you are using a compiler that supports at least C++11. Examples of such
+compilers are GCC 4.7+ and Clang 3.2+.
+
+If you are using Travis, consider either migrating your build to Ubuntu Trusty or
+installing GCC 4.8. See mini_racer's README.md for more information.
+
+
+EOS
+end
+
+CONFIG['LDSHARED'] = '$(CXX) -shared' unless IS_DARWIN
+if CONFIG['warnflags']
+  CONFIG['warnflags'].gsub!('-Wdeclaration-after-statement', '')
+  CONFIG['warnflags'].gsub!('-Wimplicit-function-declaration', '')
+end
+
+if enable_config('debug') || enable_config('asan')
+  CONFIG['debugflags'] << ' -ggdb3 -O0'
+end
+
 Libv8.configure_makefile
 
 if enable_config('asan')
