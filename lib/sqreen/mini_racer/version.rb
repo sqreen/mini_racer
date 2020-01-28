05f6dd9fd9ec0f786ccf15ff11f65bca20a8d9dc
Loic Nageleisen <loic@sqreen.io>
2020-01-28 15:50:55 +0100

    Release 0.2.5.0.1.beta3

--- lib/sqreen/mini_racer/version.rb
+++ lib/sqreen/mini_racer/version.rb
@@ -2,6 +2,6 @@ module Sqreen
 module MiniRacer
   # part before qualifier is the number of the last upstream release
   # since we synced with it
-  VERSION = "0.2.5.0.1.beta2"
+  VERSION = "0.2.5.0.1.beta3"
 end
 end
